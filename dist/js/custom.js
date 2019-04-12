d7278a93dc47f3ed92ffb3f8715672dee8d69486
RichardQin918 <richardqin918@gmail.com>
2019-04-12 17:54:57 -0400

    updating krawlcat

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -14,40 +14,54 @@ jQuery(function ($) {
     });
     
 
-if (jQuery('.image-slider').length >= 1) {
-    jQuery('.image-slider').slick({
-        arrows: false,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        draggable: true,
-        focusOnSelect: true,
-        speed: 500,
-        autoplaySpeed: 4000,
-        adaptiveHeight: false,
-        centerMode: true,
-        centerPadding: '40%',
-        slidesToShow: 1,
-        fade: false,
-        cssEase: 'ease-in-out',
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    centerPadding: '33.333%',
+    if (jQuery('.image-slider').length >= 1) {
+        jQuery('.image-slider').slick({
+            arrows: false,
+            prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+            dots: false,
+            infinite: true,
+            autoplay: true,
+            draggable: true,
+            focusOnSelect: true,
+            speed: 500,
+            autoplaySpeed: 4000,
+            adaptiveHeight: false,
+            centerMode: true,
+            centerPadding: '40%',
+            slidesToShow: 1,
+            fade: false,
+            cssEase: 'ease-in-out',
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: {
+                        centerPadding: '33.333%',
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        centerPadding: '15%',
+                    }
                 }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    centerPadding: '15%',
-                }
-            }
-        ]
+            ]
+        });
+    }
+
+    //control play
+    $(".video-overlay-play-button").click(function() {
+        if (this.style.display === "none") {
+            this.style.display = "block";
+
+        }
+        else {
+            this.style.display = "none";
+            this.parentElement.children[2].setAttribute("style", "display: none");
+            this.parentElement.children[0].play();
+
+        }
     });
-}
 
 
 });
