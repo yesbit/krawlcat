067d0c154661e94a8794ab9a304205dceb2efb1f
Poutine <richardqin918@gmail.com>
2020-07-31 10:51:48 -0400

    updating to express server

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cn', function(req, res, next) {
+  res.render('index-cn', { title: 'Express' });
+});
+
+router.get('/supernode', function(req, res, next) {
+  res.render('supernode', { title: 'Express' });
+});
+
+router.get('/supernode-cn', function(req, res, next) {
+  res.render('supernode-cn', { title: 'Express' });
+});
+
+module.exports = router;
