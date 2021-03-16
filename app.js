036d08e9154499fd383245997ca3ccdf3595d775
Richard Qin <richardqin@Richards-Mac-mini.local>
2021-03-16 13:55:39 -0400

    add fix

--- app.js
+++ app.js
@@ -32,20 +32,4 @@ app.use('/public/Swapall',express.static(path.join(__dirname, 'public/Swapall'))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
